db066cd3065e9fd907f240e051ed72bb58adbd22
Braeulias <eliasbraeuer760@gmail.com>
2024-08-28 10:43:02 +0200

    Code formated/Project finished

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,23 @@ use std::io::{self, Write};
 use std::thread::sleep;
 use std::time::Duration;
 
-fn calculate_x(i: f32, j: f32, k: f32, a: f32, b: f32, c: f32) -> f32 { //uses 3D Rotation Matrix
+fn calculate_x(i: f32, j: f32, k: f32, a: f32, b: f32, c: f32) -> f32 {
+    //uses 3D Rotation Matrix
     j * a.sin() * b.sin() * c.cos() - k * a.cos() * b.sin() * c.cos()
-        + j * a.cos() * c.sin() + k * a.sin() * c.sin()
+        + j * a.cos() * c.sin()
+        + k * a.sin() * c.sin()
         + i * b.cos() * c.cos()
 }
 
-fn calculate_y(i: f32, j: f32, k: f32, a: f32, b: f32, c: f32) -> f32 {  //uses 3D Rotation Matrix
-    j * a.cos() * c.cos() + k * a.sin() * c.cos()
-        - j * a.sin() * b.sin() * c.sin() + k * a.cos() * b.sin() * c.sin()
+fn calculate_y(i: f32, j: f32, k: f32, a: f32, b: f32, c: f32) -> f32 {
+    //uses 3D Rotation Matrix
+    j * a.cos() * c.cos() + k * a.sin() * c.cos() - j * a.sin() * b.sin() * c.sin()
+        + k * a.cos() * b.sin() * c.sin()
         - i * b.cos() * c.sin()
 }
 
-fn calculate_z(i: f32, j: f32, k: f32, a: f32, b: f32) -> f32 {  //uses 3D Rotation Matrix
+fn calculate_z(i: f32, j: f32, k: f32, a: f32, b: f32) -> f32 {
+    //uses 3D Rotation Matrix
     k * a.cos() * b.cos() - j * a.sin() * b.cos() + i * b.sin()
 }
 
@@ -34,8 +38,8 @@ fn calculate_for_surface(
     k1: f32,
     horizontal_offset: f32,
 ) {
-    let x = calculate_x(cube_x,cube_y,cube_z,a,b,c);
-    let y = calculate_y(cube_x, cube_y, cube_z,a,b,c);
+    let x = calculate_x(cube_x, cube_y, cube_z, a, b, c);
+    let y = calculate_y(cube_x, cube_y, cube_z, a, b, c);
     let z = calculate_z(cube_x, cube_y, cube_z, a, b) + distance_from_cam;
 
     if z <= 0.0 {
@@ -48,36 +52,39 @@ fn calculate_for_surface(
     let yp = (height as f32 / 2.0 + k1 * ooz * y).round() as isize;
 
     if xp < 0 || xp >= width as isize || yp < 0 || yp >= height as isize {
-        eprintln!("Out of bounds: xp={}, yp={}, width={}, height={}", xp, yp, width, height);
+        eprintln!(
+            "Out of bounds: xp={}, yp={}, width={}, height={}",
+            xp, yp, width, height
+        );
         return;
     }
 
-    let idx = (xp +yp * width as isize) as usize; //what idx to place the char in the Buffer
+    let idx = (xp + yp * width as isize) as usize; //what idx to place the char in the Buffer
 
     if let Some(z_buf) = z_buffer.get_mut(idx) {
-        if ooz > *z_buf { //checks if char is visible in rotating cube
+        if ooz > *z_buf {
+            //checks if char is visible in rotating cube
             *z_buf = ooz;
-            if let  Some(buf) = buffer.get_mut(idx) {
+            if let Some(buf) = buffer.get_mut(idx) {
                 *buf = ch; //places char
             }
         }
     }
 }
 
-
 fn main() {
-    let width = 160;    //of the Terminal
-    let height = 44;    //again of the Terminal :)
-    let mut a = 0.0;    //rotation x
-    let mut b = 0.0;    //rotation y
-    let mut c = 0.0;    //rotation z
+    let width = 160; //of the Terminal
+    let height = 44; //again of the Terminal :)
+    let mut a = 0.0; //rotation x
+    let mut b = 0.0; //rotation y
+    let mut c = 0.0; //rotation z
 
     let distance_from_cam = 100.0; //used to change size of all cubes if you have multiple
     let k1 = 40.0;
     let increment_speed = 0.6; //speed of cube
 
-    let mut buffer = vec![' '; width * height];     //cube array
-    let mut z_buffer = vec![0.0; width * height];   //visible? array
+    let mut buffer = vec![' '; width * height]; //cube array
+    let mut z_buffer = vec![0.0; width * height]; //visible? array
 
     print!("\x1b[2J");
     let stdout = io::stdout();
@@ -88,7 +95,7 @@ fn main() {
         z_buffer.fill(0.0);
 
         //change it you want more cubes
-        let cube_width = 15.0;  //Change cube size
+        let cube_width = 15.0; //Change cube size
         let horizontel_offset = -2.0 * cube_width; //only important if you want multiple cubes
 
         //cube
@@ -96,14 +103,102 @@ fn main() {
         while cube_x < cube_width {
             let mut cube_y = -cube_width;
             while cube_y < cube_width {
-
-                calculate_for_surface(-cube_x, cube_y, cube_width, '#', &mut buffer, &mut z_buffer, width, height, a, b, c, distance_from_cam, k1, horizontel_offset);
-                calculate_for_surface(cube_x, cube_y, -cube_width, '@', &mut buffer, &mut z_buffer, width, height, a, b, c, distance_from_cam, k1, horizontel_offset);
-                calculate_for_surface(cube_width, cube_y, cube_x, '$', &mut buffer, &mut z_buffer, width, height, a, b, c, distance_from_cam, k1, horizontel_offset);
-                calculate_for_surface(-cube_width, cube_y, -cube_x, '~', &mut buffer, &mut z_buffer, width, height, a, b, c, distance_from_cam, k1, horizontel_offset);
-                calculate_for_surface(cube_x, cube_width, cube_y, '+', &mut buffer, &mut z_buffer, width, height, a, b, c, distance_from_cam, k1, horizontel_offset);
-                calculate_for_surface(cube_x, -cube_width, -cube_y, ';', &mut buffer, &mut z_buffer, width, height, a, b, c, distance_from_cam, k1, horizontel_offset);
-
+                calculate_for_surface(
+                    -cube_x,
+                    cube_y,
+                    cube_width,
+                    '#',
+                    &mut buffer,
+                    &mut z_buffer,
+                    width,
+                    height,
+                    a,
+                    b,
+                    c,
+                    distance_from_cam,
+                    k1,
+                    horizontel_offset,
+                );
+                calculate_for_surface(
+                    cube_x,
+                    cube_y,
+                    -cube_width,
+                    '@',
+                    &mut buffer,
+                    &mut z_buffer,
+                    width,
+                    height,
+                    a,
+                    b,
+                    c,
+                    distance_from_cam,
+                    k1,
+                    horizontel_offset,
+                );
+                calculate_for_surface(
+                    cube_width,
+                    cube_y,
+                    cube_x,
+                    '$',
+                    &mut buffer,
+                    &mut z_buffer,
+                    width,
+                    height,
+                    a,
+                    b,
+                    c,
+                    distance_from_cam,
+                    k1,
+                    horizontel_offset,
+                );
+                calculate_for_surface(
+                    -cube_width,
+                    cube_y,
+                    -cube_x,
+                    '~',
+                    &mut buffer,
+                    &mut z_buffer,
+                    width,
+                    height,
+                    a,
+                    b,
+                    c,
+                    distance_from_cam,
+                    k1,
+                    horizontel_offset,
+                );
+                calculate_for_surface(
+                    cube_x,
+                    cube_width,
+                    cube_y,
+                    '+',
+                    &mut buffer,
+                    &mut z_buffer,
+                    width,
+                    height,
+                    a,
+                    b,
+                    c,
+                    distance_from_cam,
+                    k1,
+                    horizontel_offset,
+                );
+                calculate_for_surface(
+                    cube_x,
+                    -cube_width,
+                    -cube_y,
+                    ';',
+                    &mut buffer,
+                    &mut z_buffer,
+                    width,
+                    height,
+                    a,
+                    b,
+                    c,
+                    distance_from_cam,
+                    k1,
+                    horizontel_offset,
+                );
 
                 cube_y += increment_speed;
             }
@@ -127,6 +222,4 @@ fn main() {
         c += 0.01;
         sleep(Duration::from_millis(16));
     }
-
-
 }
